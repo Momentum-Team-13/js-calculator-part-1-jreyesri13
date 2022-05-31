3c3dfd5fad93c1ab9872d0f6aefe9a3fd85c45a1
Jose Reyes <jreyesri@cbu.edu>
2022-05-30 23:57:46 -0400

    Calculator is working

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
+let display = document.getElementById("display")
+console.log(display)
+
 let buttons = document.querySelectorAll(".button")
 console.log(buttons)
 
 for (let button of buttons) {
-    button.addEventListener("click", function (event) {
-        console.log(event.target.textContent);
+    button.addEventListener("click", (event) => {
+        console.log(event.target.innerText);
+
+        if (event.target.innerText != "=") {
+            display.innerText += event.target.innerText
+            // console.log(event.target.innerText)
+        }
+
+        if (event.target.innerText == "AC") {
+            display.innerText = ""
+            // console.log(event.target.innerText)
+        }
+
+        if (event.target.innerText == "=") {
+            try {
+                display.innerText = eval(display.innerText)
+            }
+            catch {
+                display.innerText = "Error"
+            }
+            console.log(display.innerText)
+            // console.log(event.target.innerText)
+        }
     })
 }
-
-// let newElement = document.createElement("p");
-// // create a new element, in this case a p
-// newElement.innerText = event.target.textContent;
-// display.appendChild(newElement);
-//   })
-// }
-
